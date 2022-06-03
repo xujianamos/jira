8a9391968fb7254c423e30900df1935d01b26931
xujian <2513573453@qq.com>
2022-06-03 16:26:05 +0800

    feat:添加useDebounce和useMount两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -4,27 +4,29 @@ import { cleanObject } from "../../utils";
 
 import { List } from "./list";
 import { SearchPanel } from "./search-panel";
-
+import { useMount } from "../../hooks/useMount";
+import { useDebounce } from "../../hooks/useDebounce";
 const apiUrl = process.env.REACT_APP_API_URL;
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: "",
     personId: "",
   });
+  const debouncedparam = useDebounce(param, 2000);
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (res) => {
-        if (res.ok) {
-          setList(await res.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedparam))}`
+    ).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json());
       }
-    );
-  }, [param]);
+    });
+  }, [debouncedparam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
         setUsers(await res.json());
