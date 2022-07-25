32f4e7a35d38a584bf19dc95fd5ff4b87d437094
徐建 <xujian@cnksi.com>
2022-07-25 11:44:40 +0800

    feat:添加ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import "./App.less";
 import { useAuth } from "./context/auth-context";
 import { AuthenticatedApp } from "./authenticated-app";
 import { UnauthenticatedApp } from "./unauthenticated-app";
-
+import { ErrorBoundary } from "./components/error-boundary";
+import { FullPageErrorFallback } from "components/lib";
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
